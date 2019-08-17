091290ea3abea0dc39f845137ab4fba65923c32e
yumartins <yuri@estudioflow.com.br>
2019-08-17 11:50:06 -0300

    add capture position card for moving

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container } from './styles';
+import BoardContext from './context';
 import { loadLists } from '../../services/api';
 import List from '../List';
 
-const lists = loadLists();
+const data = loadLists();
 
 export default function Board() {
 
+    const [lists, setLists] = useState(data);
+
+    function move(from, to) {
+        console.log(from, to)
+    }
+
     return (
       
-        <Container>
-            { lists.map(list => <List key = { list.title } data = { list } />) }
-        </Container>
+        <BoardContext.Provider value = {{ lists, move }} >
+            <Container>
+                { lists.map(list => <List key = { list.title } data = { list } />) }
+            </Container>
+        </BoardContext.Provider>
 
     );
 
